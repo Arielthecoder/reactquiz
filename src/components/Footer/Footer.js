7ff5131dbe9c63de92c024c78b71b2d73238be4f
Arielthecoder <ahermitt02@gmail.com>
2023-09-27 12:42:48 -0400

    fix css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by Ari
+      <a
+        href="https://www.linkedin.com/in/arielhermittcodes/"
+        style={{ cursor: "pointer" }}
+      >
+   
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
